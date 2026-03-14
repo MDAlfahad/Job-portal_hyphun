13660c13b966afab53c2cc0ecd01e05a42d87b59
MD Alfahad <alfahadkhan715@gmail.com>
2026-03-14 14:19:23 +0530

    some changes

--- frontend/src/components/Pages/HeroContaner/CrauselContainer.jsx
+++ frontend/src/components/Pages/HeroContaner/CrauselContainer.jsx
@@ -1,16 +1,19 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import CrauselCard from "../../Components/Cards/CrauselCard"
 
 const CrauselContainer = ()=>{
 
     return(
         <>
-        <div className="w-full max-w-[1800px] px-4 md:px-12 py-12 ">
-            <div className="w-full p-3 shadow-xl flex  gap-4 ">
+        <div className="w-full max-w-[1800px] m-auto px-4 md:px-12 py-12 flex items-center">
+            <button><ChevronLeft strokeWidth={1.5} /></button>
+            <div className="w-full shadow-xl flex  gap-4 ">
                 <CrauselCard/>
                 <CrauselCard/>
                 <CrauselCard/>
                 <CrauselCard/>
             </div>
+            <button><ChevronRight strokeWidth={1.5} /></button>
         </div>
         </>
     )
